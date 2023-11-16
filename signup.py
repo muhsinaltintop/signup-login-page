21740e3fc92efea59ba0b90a7bc0958b6a2aeaa7
Muhsin Altintop <muhsin.altintop@gmail.com>
2023-11-17 01:50:15 +0300

    signup logic created

---
+++ signup.py
@@ -0,0 +1,9 @@
+#signup
+nameLastName = input("Please enter your Name and Lastname. ")
+username = input("Please choose a username: ")
+password = input("Please choose a password")
+
+if nameLastName and username and password :
+    print("Successful")
+else:
+    print("Unsuccesful")
